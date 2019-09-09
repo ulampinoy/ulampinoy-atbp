4e57a5df9bdf4b51753a9d405cc026aa5823e69c
Allan Reyes <chef@ulampinoy.com>
2019-09-09 10:18:14 +0200

    Support for lfs and remove cms

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,13 +1,3 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
-
 getCatList = function(collection) {
   let catSet = new Set()
 
